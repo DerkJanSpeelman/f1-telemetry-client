e2632061b04a91f5404e46c9f7b2b5ae18b15a34
Derk Jan Speelman <derkjanspeelman@gmail.com>
2020-02-14 16:30:42 +0100

    Fix m_sessionUID type in PacketHeader interface

--- build/src/parsers/packets/types.d.ts
+++ build/src/parsers/packets/types.d.ts
@@ -154,7 +154,7 @@ export interface PacketHeader {
     m_packetFormat: number;
     m_packetVersion: number;
     m_packetId: number;
-    m_sessionUID: number;
+    m_sessionUID: bigint;
     m_sessionTime: number;
     m_frameIdentifier: number;
     m_playerCarIndex: number;
